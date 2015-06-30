579829841d6ba9038e20bde0297e01fdc3688cd9
Vladislav Stanisheskij <Vladislav-work1909@yandex.by>
2015-07-01 01:24:48 +0300

    Create project

---
+++ Logic_Task/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Logic_Task
+//
+//  Created by Владислав Станишевский on 7/1/15.
+//  Copyright (c) 2015 Vlad Stanishevskij. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Logic_Task/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Logic_Task
+//
+//  Created by Владислав Станишевский on 7/1/15.
+//  Copyright (c) 2015 Vlad Stanishevskij. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
